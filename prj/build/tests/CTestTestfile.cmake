65861707e01497a71044b5d090ebd02c0b897162
kgliwinski <72801358+kgliwinski@users.noreply.github.com>
2021-04-24 23:34:34 +0200

    Delete prj/build

--- prj/build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krzysztof/PO/zad3-kgliwinski/prj/tests
-# Build directory: /home/krzysztof/PO/zad3-kgliwinski/prj/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(engine.unit_tests "/home/krzysztof/PO/zad3-kgliwinski/prj/build/unit_tests")
-set_tests_properties(engine.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/krzysztof/PO/zad3-kgliwinski/prj/tests/CMakeLists.txt;26;add_test;/home/krzysztof/PO/zad3-kgliwinski/prj/tests/CMakeLists.txt;0;")
